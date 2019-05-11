fe62c889f737754c679f781c94bf2f4ddea303fc
ymgw0867 <kyamagiwa@gmail.com>
2019-05-11 10:17:10 +0900

    giyhub upload

--- FunctionApp1/Function1.cs
+++ FunctionApp1/Function1.cs
@@ -1,17 +1,31 @@
 using System;
+using System.Configuration;
 using System.IO;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Line.Messaging;
+using Line.Messaging.Webhooks;
 
 namespace FunctionApp1
 {
     public static class Function1
     {
+        static LineMessagingClient lineMessagingClient;
+
+        static Function1()
+        {
+            lineMessagingClient = new LineMessagingClient(System.Configuration.ConfigurationManager.AppSettings["ChannelAccessToken"]);
+            var sp = ServicePointManager.FindServicePoint(new Uri("https://api.line.me"));
+            sp.ConnectionLeaseTimeout = 60 * 1000;
+        }
+
         [FunctionName("Function1")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
---
+++ FunctionApp1/LineBotApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Line.Messaging;
+using Line.Messaging.Webhooks;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace FunctionApp1
+{
+    class LineBotApp : WebhookApplication
+    {
+        private LineMessagingClient MessagingClient { get; }
+        //private TableStorage<BotStatus> SourceState { get; }
+        private TraceWriter Log { get; }
+    }
+}
